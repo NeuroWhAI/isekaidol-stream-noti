d77e8e5e1c82c377fd12a83929a39fab774ca0fa
NeuroWhAI <tlsehdgus0212@gmail.com>
2022-04-14 21:07:30 +0900

    🐛 Wait until all promises be settled

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -23,6 +23,8 @@ exports.watchStreams = functions.pubsub.schedule('every 1 minutes').onRun(async 
         return null;
     }
 
+    let jobs = [];
+
     const authProvider = new ClientCredentialsAuthProvider(clientId, clientSecret);
     const apiClient = new ApiClient({ authProvider });
 
@@ -50,9 +52,10 @@ exports.watchStreams = functions.pubsub.schedule('every 1 minutes').onRun(async 
             || dbData.title !== newData.title
             || dbData.category !== newData.category
         ) {
-            refStream.set(newData)
+            let dbJob = refStream.set(newData)
                 .then(() => functions.logger.info("Stream data updated."))
                 .catch((err) => functions.logger.error("Fail to update the stream data.", err));
+            jobs.push(dbJob);
 
             let message = {
                 data: {
@@ -62,17 +65,20 @@ exports.watchStreams = functions.pubsub.schedule('every 1 minutes').onRun(async 
                 },
                 topic: member.id,
             };
-            admin.messaging().send(message)
+            let msgJob = admin.messaging().send(message)
                 .then((res) => functions.logger.info("Messaging success.", message, res))
                 .catch((err) => functions.logger.error("Messaging fail.", message, err));
+            jobs.push(msgJob);
         }
     }
+
+    await Promise.allSettled(jobs);
 
     //response.send("Hello from Firebase!");
     return null;
 });
 
-exports.updateSub = functions.database.ref('/users/{user}').onWrite((snapshot, context) => {
+exports.updateSub = functions.database.ref('/users/{user}').onWrite(async (snapshot, context) => {
     let user = context.params.user
         .replace(/!!!1!!!/g, '/')
         .replace(/!!!2!!!/g, '.')
@@ -84,19 +90,25 @@ exports.updateSub = functions.database.ref('/users/{user}').onWrite((snapshot, c
 
     functions.logger.info("Update subs.", subs, user);
 
+    let jobs = [];
+
     subs = subs.split(',');
     for (let id of subs) {
-        admin.messaging().subscribeToTopic(user, id)
+        let job = admin.messaging().subscribeToTopic(user, id)
             .then((res) => functions.logger.info("Subscribe success.", id, res))
             .catch((err) => functions.logger.error("Subscribe fail.", id, err));
+        jobs.push(job);
     }
     for (let member of members) {
         if (subs.indexOf(member.id) < 0) {
-            admin.messaging().unsubscribeFromTopic(user, member.id)
+            let job = admin.messaging().unsubscribeFromTopic(user, member.id)
                 .then((res) => functions.logger.info("Unsubscribe success.", member.id, res))
                 .catch((err) => functions.logger.error("Unsubscribe fail.", member.id, err));
+            jobs.push(job);
         }
     }
+
+    await Promise.allSettled(jobs);
 
     return null;
 });
