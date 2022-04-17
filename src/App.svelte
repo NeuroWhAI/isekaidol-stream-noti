e6b6ee8d547125443b605fd481b959d4a703f4af
NeuroWhAI <tlsehdgus0212@gmail.com>
2022-04-17 18:09:22 +0900

    ✨ Add the Telegram channel buttons

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { Headline, Subhead } from 'attractions';
+    import { Headline, Subhead, Button } from 'attractions';
     import MemberCard from './components/MemberCard.svelte';
 
     import { ref, onValue, set, get } from "firebase/database";
@@ -311,6 +311,19 @@
             loading={configLoadings[id]}
         />
     {/each}
+    <div class="channel-title">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telegram" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z"/>
+        </svg>
+        <span>&nbsp;텔레그램 채널에서도 알려드려요.</span>
+    </div>
+    <div class="channel-box">
+        {#each memberIds as id}
+            <Button round small href="https://t.me/{id}_stream_noti" target="_blank">
+                <img class="channel-img" alt="{id}" src="image/{id}.png" />
+            </Button>
+        {/each}
+    </div>
 </main>
 
 <hr />
@@ -346,6 +359,41 @@
         padding: 0;
     }
 
+    .header {
+        margin-bottom: 20px;
+    }
+
+    .channel-title {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: baseline;
+        margin: 24px 0 0 0;
+    }
+    @media (max-width: 600px) {
+        .channel-title {
+            font-size: 0.9em;
+        }
+    }
+    @media (max-width: 520px) {
+        .channel-title {
+            font-size: 0.8em;
+        }
+    }
+
+    .channel-box {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+    }
+    
+    .channel-img {
+        width: 24px;
+        height: 24px;
+        border-radius: 50%;
+        box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+    }
+
     footer {
         text-align: center;
         font-size: 0.6em;
@@ -355,9 +403,5 @@
 
     footer p {
         margin: 0;
-    }
-
-    .header {
-        margin-bottom: 20px;
     }
 </style>
