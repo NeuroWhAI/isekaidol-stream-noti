99ea3da9f8a46366fc8a9cc032f55a8eb561e503
NeuroWhAI <tlsehdgus0212@gmail.com>
2022-05-26 21:19:17 +0900

    ⬆️ Upgrade worker dependencies

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.8.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.8.1/firebase-messaging-compat.js');
 
 self.addEventListener('notificationclick', function(e) {
     const members = {
