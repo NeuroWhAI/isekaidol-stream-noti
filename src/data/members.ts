bf9324e85f20bee03ded756c725ae4d61e0061bd
NeuroWhAI <tlsehdgus0212@gmail.com>
2022-12-21 20:48:50 +0900

    💄 Change personal color

--- src/data/members.ts
+++ src/data/members.ts
@@ -5,11 +5,11 @@ class MemberData {
 }
 
 const members: { [id: string]: MemberData } = {
-    'jururu': new MemberData('주르르', 'cotton__123', '#800080', '6.10'),
+    'jururu': new MemberData('주르르', 'cotton__123', '#ffacac', '6.10'),
     'jingburger': new MemberData('징버거', 'jingburger', '#f0a957', '10.8'),
     'viichan': new MemberData('비챤', 'viichan6', '#85ac20', '1.16'),
     'gosegu': new MemberData('고세구', 'gosegugosegu', '#467ec6', '7.27'), // 생일 대신 첫 방송일.
-    'lilpa': new MemberData('릴파', 'lilpaaaaaa', '#000080', '3.9'),
+    'lilpa': new MemberData('릴파', 'lilpaaaaaa', '#3e52d9', '3.9'),
     'ine': new MemberData('아이네', 'vo_ine', '#8a2be2', '7.26'), // 생일 대신 첫 방송일.
 };
 
