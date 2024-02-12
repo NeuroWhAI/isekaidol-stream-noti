ea07a0d222dae70c11ac689cfc9f179844d6e1ad
NeuroWhAI <tlsehdgus0212@gmail.com>
2024-02-12 20:19:41 +0900

    ✨ Add a multi-view button

--- src/App.svelte
+++ src/App.svelte
@@ -57,9 +57,19 @@
         configLoadings[id] = false;
     }
 
+    let multiStreamUrl = "https://mul.live/";
+
     // 방송 정보가 바뀌면 받아서 갱신.
     FireDb.onValue(FireDb.ref(database, 'afreeca'), (snapshot) => {
         streamData = snapshot.val();
+
+        // 멀티스트림 링크 갱신.
+        multiStreamUrl = "https://mul.live";
+        for (let id of memberIds) {
+            if (streamData[id].online) {
+                multiStreamUrl += "/a:" + members[id].afreecaId;
+            }
+        }
     });
 
     let configAvailable = true;
@@ -511,6 +521,12 @@
             dday={ddays[id]}
         />
     {/each}
+    <div class="multi-stream">
+        <Button outline small href={multiStreamUrl} title="방송을 한 화면에 모아서 봅니다.">
+            <ExternalLinkIcon size="20" />
+            멀티뷰에서 보기
+        </Button>
+    </div>
     <div class="channel-title">
         <svg width="16" height="16" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Telegram</title><path d="M11.944 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12 12 12 0 0 0 12-12A12 12 0 0 0 12 0a12 12 0 0 0-.056 0zm4.962 7.224c.1-.002.321.023.465.14a.506.506 0 0 1 .171.325c.016.093.036.306.02.472-.18 1.898-.962 6.502-1.36 8.627-.168.9-.499 1.201-.82 1.23-.696.065-1.225-.46-1.9-.902-1.056-.693-1.653-1.124-2.678-1.8-1.185-.78-.417-1.21.258-1.91.177-.184 3.247-2.977 3.307-3.23.007-.032.014-.15-.056-.212s-.174-.041-.249-.024c-.106.024-1.793 1.14-5.061 3.345-.48.33-.913.49-1.302.48-.428-.008-1.252-.241-1.865-.44-.752-.245-1.349-.374-1.297-.789.027-.216.325-.437.893-.663 3.498-1.524 5.83-2.529 6.998-3.014 3.332-1.386 4.025-1.627 4.476-1.635z"/></svg>
         <span>&nbsp;텔레그램 채널에서도 알려드려요.</span>
@@ -639,6 +655,16 @@
         }
     }
 
+    .multi-stream {
+        display: flex;
+        justify-content: center;
+    }
+    .multi-stream > :global(a.btn) {
+        font-family: inherit;
+        font-size: inherit;
+        font-weight: inherit;
+    }
+
     .channel-title {
         margin: 24px 0 0 0;
     }
@@ -652,12 +678,12 @@
         align-items: baseline;
     }
     @media (max-width: 600px) {
-        .channel-title, .webhook-title {
+        .channel-title, .webhook-title, .multi-stream {
             font-size: 0.9em;
         }
     }
     @media (max-width: 520px) {
-        .channel-title, .webhook-title {
+        .channel-title, .webhook-title, .multi-stream {
             font-size: 0.8em;
         }
     }
